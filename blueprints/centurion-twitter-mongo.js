119210e1c7a4d712aca1c04b6fd2b0f239ad1a36
taniki <tk@deveha.com>
2012-11-20 16:50:52 +0100

    add twitter persons

--- blueprints/centurion-twitter-mongo.js
+++ blueprints/centurion-twitter-mongo.js
@@ -2,21 +2,25 @@ var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3010');
 
 var mongo = require('mongodb');
-var db = new mongo.Db('twitter', new mongo.Server('localhost', 27017, {auto_reconnect: true}));
 
-var count = 0;
+module.exports = function(personality){
 
-socket.on('connection', function(){
-	socket.emit('join_room', 'warehouse');
-});
+	var db = new mongo.Db('twitter', new mongo.Server(personality.mongo.host, personality.mongo.port, {auto_reconnect: true}));
 
-db.collection('tweets', function(err,tweets){
-	socket.on('tweet', function(data){
-		tweets.update({ id_str : data.id_str }, data, {upsert:true }, function(err, result){
+	this.count = 0;
+
+	socket.on('connection', function(){
+		socket.emit('join_room', 'warehouse');
+	});
+
+	db.collection('tweets', function(err,tweets){
+		socket.on('tweet', function(data){
+			tweets.update({ id_str : data.id_str }, data, {upsert:true }, function(err, result){
+			});
 		});
 	});
-});
 
-socket.on('welcome', function(server){
-	console.log('[ '+ server.name +' ] dans la place !');
-});
+	socket.on('welcome', function(server){
+		console.log('[ '+ server.name +' ] dans la place !');
+	});
+}
